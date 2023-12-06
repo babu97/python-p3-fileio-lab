dffa4897ea4e527162c6ec25051055e449d94e16
babu97 <benkaimugul@gmail.com>
2023-12-06 23:35:24 +0000

    finished my work

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,13 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", "w") as text:
+        text.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", "a") as text:
+        text.write(append_content)
+
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt") as texts:
+        return texts.read()
